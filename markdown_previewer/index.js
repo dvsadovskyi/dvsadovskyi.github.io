5fabf19a730d0bfa85f748e2e3af8b7664e5b5ed
dvsadovskyi_LOCAL_WORK <dvsadovskyi>
2023-06-15 09:45:05 +0300

    upd

--- markdown_previewer/index.js
+++
@@ -1,8 +0,0 @@
-const App = () => {
-    return (
-        <div className="container">In porgress</div >
-    )
-}
-
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(<App />)
